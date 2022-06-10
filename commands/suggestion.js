8459fb2fdca2627bf29e7180a67abdfae9fabfa0
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-06-10 21:49:09 +0000

    revert

--- commands/suggestion.js
+++ commands/suggestion.js
@@ -1,29 +1,20 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageActionRow, Modal, TextInputComponent } = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, Modal, TextInputComponent } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("suggestion")
-    .setDescription("give us suggestions"),
-  async execute(interaction, client) {
-    const modal = new Modal()
-			.setCustomId("suggestions")
-			.setTitle("Suggestions");
-    const Input = new TextInputComponent()
-      .setCustomId("Input")
-      .setLabel("What’s Your Suggestion?")
-      .setStyle("PARAGRAPH");
-    const firstActionRow = new MessageActionRow().addComponents(Input);
-    modal.addComponents(firstActionRow);
-    await interaction.showModal(modal);
-
-		client.on('interactionCreate', interaction => {
-			if (!interaction.isModalSubmit()) return;
-			const input = interaction.fields.getTextInputValue('Input');
-			const type = interaction.customId
-			db.set(input, type);
-		});
-
-		await modalSubmit.reply('thank you.');
-  },
+	data: new SlashCommandBuilder()
+		.setName('suggestion')
+		.setDescription('give us suggestions'),
+	async execute(interaction) {
+		const modal = new Modal()
+			.setCustomId('suggestions')
+			.setTitle('Suggestions');
+		const Input = new TextInputComponent()
+			.setCustomId('Input')
+			.setLabel("What’s Your Suggestion?")
+			.setStyle('PARAGRAPH');
+		const firstActionRow = new MessageActionRow().addComponents(Input);
+		modal.addComponents(firstActionRow);
+		await interaction.showModal(modal);
+	},
 };
