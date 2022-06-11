bfc5574d216977c3a2583127c1f5761e872c7fd9
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-06-10 21:18:38 -0700

    uhm

--- commands/ask-ibby.js
+++ commands/ask-ibby.js
@@ -10,11 +10,12 @@ module.exports = {
 				.setDescription('What is your question to ibby?')
 				.setRequired(true)),
 	async execute(interaction) {
+		let usered = interaction.guild.members.cache.get('816911203036692511');
 		const embed = new MessageEmbed()
 			.setColor('#33ccff')
 			.setTitle('Question sent! You asked:')
 			.setDescription(interaction.options.getString('input') + '\n\nMake sure to have your DMs open + or have IB unblocked for you to get your answer!')
-		await interaction.user.fetch('816911203036692511').send('<@' + interaction.user.id + '> asks you: **' + interaction.options.getString('input') + '**\n\n**' + interaction.user.username + '**’s ID:\n\n' + interaction.user.id);
-		await interaction.reply({ embeds: [embed] });
+		usered.send('<@' + interaction.user.id + '> asks you: **' + interaction.options.getString('input') + '**\n\n**' + interaction.user.username + '**’s ID:\n\n' + interaction.user.id);
+		return interaction.reply({ embeds: [embed] });
 	},
 };
--- commands/userinfo.js
+++ commands/userinfo.js
@@ -10,7 +10,7 @@ module.exports = {
 			.setColor('#33ccff')
 			.setTitle(interaction.options.getUser('target') + '’s Info:')
 			.setDescription('Tag: ' + interaction.user.tag + '\nDiscriminator (Tag Alone): ' + interaction.user.discriminator + '\n\nYour ID: ', interaction.user.id);
-		await interaction.user.fetch('id').send({ embeds: [embed] });
+		await interaction.user.send({ embeds: [embed] });
 		await interaction.channels.cache.get(process.env.suschannel).send({ embeds: [embed] });
 		await interaction.reply(String('Sent in DMs!'));
 	},
