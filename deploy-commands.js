8523088e3551ccc8e4f8b045e04125eef6597727
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-08-14 22:06:11 -0700

    ga me2

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,3 +1,4 @@
+const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { readdirSync } = require('fs');
@@ -10,18 +11,30 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
+const commands2 = [
+	new SlashCommandBuilder().setName('game-test2').setDescription('another goddamn test yipee'),
+]
+	.map(command => command.toJSON());
+
+
 const rest = new REST({ version: '9' }).setToken(process.env.token);
+
+const rest2 = new REST({ version: '10' }).setToken(process.env.token);
+
+rest2.put(Routes.applicationGuildCommands(process.env.clientId), { body: commands2 })
+	.then(() => console.log('Successfully registered application commands. (batch 1)'))
+	.catch(console.error);
 
 (async () => {
 	try {
-		console.log('Started refreshing application (/) commands.');
+		console.log('Started refreshing application (/) commands. (batch 2)');
 
 		await rest.put(
 			Routes.applicationCommands(process.env.clientId),
 			{ body: commands },
 		);
 
-		console.log('Successfully reloaded application (/) commands.');
+		console.log('Successfully reloaded application (/) commands. (batch 2)');
 	} catch (error) {
 		console.error(error);
 	}
