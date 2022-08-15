0f3ddd376783b752091c1477a1f6158ca1dd5200
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-08-15 05:08:34 +0000

    jk

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,3 @@
-const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { readdirSync } = require('fs');
@@ -11,30 +10,18 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const commands2 = [
-	new SlashCommandBuilder().setName('game-test2').setDescription('another goddamn test yipee'),
-]
-	.map(command => command.toJSON());
-
-
 const rest = new REST({ version: '9' }).setToken(process.env.token);
-
-const rest2 = new REST({ version: '10' }).setToken(process.env.token);
-
-rest2.put(Routes.applicationGuildCommands(process.env.clientId), { body: commands2 })
-	.then(() => console.log('Successfully registered application commands. (batch 1)'))
-	.catch(console.error);
 
 (async () => {
 	try {
-		console.log('Started refreshing application (/) commands. (batch 2)');
+		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
 			Routes.applicationCommands(process.env.clientId),
 			{ body: commands },
 		);
 
-		console.log('Successfully reloaded application (/) commands. (batch 2)');
+		console.log('Successfully reloaded application (/) commands.');
 	} catch (error) {
 		console.error(error);
 	}
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const keepAlive = require("./server.js");
 const { readdirSync } = require('fs');
-const { Client, Collection, Intents, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const client2 = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageReactions], });
 const Database = require("easy-json-database");
 const devMode = typeof __E_IS_DEV !== "undefined" && __E_IS_DEV;
 const db = new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/database/db.json`)
@@ -18,10 +17,6 @@ for (const file of commandFiles) {
 client.on('ready', async () => {
 	client.channels.cache.get(process.env.connected).send('yo im onlin');
 	client.user.setActivity('/BB | Slash Commands!', { type: 'PLAYING' });
-});
-
-client2.once('ready', () => {
-	console.log('Ready!');
 });
 
 keepAlive()
@@ -56,47 +51,4 @@ client.on('interactionCreate', async interaction => {
 	db.set(input, type);
 });
 
-client2.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	if (interaction.commandName === 'game-test2') {
-		const message = await interaction.reply({ content: 'Reacting with letters', fetchReply: true });
-		const emojiCharacters = require('../emojiCharacters.js');
-		message.react(emojiCharacters.a)
-			.then(() => message.react(emojiCharacters.b))
-			.then(() => message.react(emojiCharacters.c))
-			.then(() => message.react(emojiCharacters.d))
-			.catch(error => console.error('One of the emojis failed to react:', error));
-
-		const filter = (reaction, user) => {
-			return [emojiCharacters.a, emojiCharacters.b, emojiCharacters.c, emojiCharacters.d, emojiCharacters.e, emojiCharacters.f, emojiCharacters.g].includes(reaction.emoji.name) && user.id === interaction.user.id;
-		};
-
-		message.awaitReactions({ filter, max: 1, time: 60000, errors: ['time'] })
-			.then(collected => {
-				const reaction = collected.first();
-
-				if (reaction.emoji.name === emojiCharacters.a) {
-					interaction.followUp('You reacted with A.');
-				} else if (reaction.emoji.name === emojiCharacters.b) {
-					interaction.followUp('You reacted with B.');
-				} else if (reaction.emoji.name === emojiCharacters.c) {
-					interaction.followUp('You reacted with C.');
-				} else if (reaction.emoji.name === emojiCharacters.d) {
-					interaction.followUp('You reacted with D.');
-				} else if (reaction.emoji.name === emojiCharacters.e) {
-					interaction.followUp('You reacted with E.');
-				} else if (reaction.emoji.name === emojiCharacters.f) {
-					interaction.followUp('You reacted with F.');
-				} else {
-					interaction.followUp('You reacted with G.');
-				}
-			})
-			.catch(() => {
-				interaction.followUp('Game Cancelled.');
-			});
-	}
-});
-
 client.login(process.env.token);
-client2.login(process.env.token);
