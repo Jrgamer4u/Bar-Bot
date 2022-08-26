6d4a471b5c333c566046c0845f42b8fc30e19f32
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-08-26 06:41:00 +0000

    clean up interactionCreate and index

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,21 +1,7 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, InteractionType, GatewayIntentBits } = require('discord.js');
+const { InteractionType } = require('discord.js');
 const Database = require("easy-json-database");
 const devMode = typeof __E_IS_DEV !== "undefined" && __E_IS_DEV;
-const db = new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/db.json`),
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, '../commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
-}
+const db = new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/database/db.json`);
 
 module.exports = {
 	name: 'interactionCreate',
