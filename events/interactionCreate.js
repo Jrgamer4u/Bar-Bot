4cd230bb8da10237dac5144a095cc1453a047def
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-10-29 04:54:24 +0000

    man

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,7 +7,7 @@ module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
 		if (interaction.type === InteractionType.ApplicationCommand){
-			const command = client.commands.get(interaction.commandName);
+			const command = interaction.client.commands.get(interaction.commandName);
 
 			if (!command) return;
 
