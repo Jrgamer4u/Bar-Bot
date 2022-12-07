5f479a656a88d10ebe6c76d2602a0cd7c646d25c
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-12-06 19:54:23 -0800

    remove message

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		client.channels.cache.get(process.env.connected).send('yo im onlin');
 		client.user.setActivity('/BB | Slash Commands!');
 	},
 };
