7a0f87d28c796b1645d4909f38365dece45fee09
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-12-06 19:55:13 -0800

    remove message

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		client.channels.cache.get(process.env.connected).send('Connected');
 		client.user.setActivity('RC 2 | Slash Commands!');
 	},
 };
